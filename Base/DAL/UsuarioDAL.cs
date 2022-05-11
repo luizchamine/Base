c5aaaee0cae623cf1e9beb0f6615a25e1e45fc7a
araujoluiz1725@gmail.com <araujoluiz1725@gmail.com>
2022-05-10 21:38:00 -0300

    hora e data: 21:37 10/05/2022

---
+++ Base/DAL/UsuarioDAL.cs
@@ -0,0 +1,59 @@
+﻿using Model;
+using System;
+using System.Data.SqlClient;
+using System.Data;
+
+namespace DAL
+{
+    public class UsuarioDAL
+    {
+        public Usuario Inserir(Usuario _usuario)
+        {
+            SqlConnection cn = new SqlConnection();
+            try
+            {
+                cn.ConnectionString = "";
+                SqlCommand cmd = new SqlCommand();
+                cmd.Connection = cn;
+                cmd.CommandType = CommandType.StoredProcedure;
+                cmd.CommandText = "SP_InserirUsuario";
+
+                SqlParameter pativo = new SqlParameter("@ativo", SqlDbType.Bit);
+                pativo.Value = _usuario.Ativo;
+                cmd.Parameters.Add(pativo);
+
+                SqlParameter pnomeUsuario = new SqlParameter("@NomeUsuario", SqlDbType.VarChar);
+                pativo.Value = _usuario.NomeUsuario;
+                cmd.Parameters.Add(pnomeUsuario);
+
+
+                SqlParameter psenha = new SqlParameter("@Senha", SqlDbType.VarChar);
+                psenha.Value = _usuario.Senha;
+                cmd.Parameters.Add(psenha);
+
+                SqlParameter pid = new SqlParameter("@Id", SqlDbType.Int);
+                psenha.Value = _usuario.Id;
+                cmd.Parameters.Add(pid);
+
+                cn.Open();
+                _usuario.Id = Convert.ToInt32(cmd.ExecuteScalar());
+
+                return _usuario;
+            }
+            catch (SqlException ex)
+            {
+                throw new Exception("Servidor SQL Erro: " + ex.Message);
+            }
+
+            catch (Exception ex)
+            {
+
+                throw new Exception(ex.Message);
+            }
+            finally
+            {
+                cn.Close();
+            }
+        }
+    }
+}
