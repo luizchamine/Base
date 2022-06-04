d7fda1f0eae2b02207044ca20250a3410bc1cde7
luizchamine <araujoluiz1725@gmail.com>
2022-06-03 21:39:26 -0300

    criadas as classes DAL, BLL e Model. classe Model já programada por completo. iniciado a programação da classe DAL.

---
+++ Base/DAL/TarefaDAL.cs
@@ -0,0 +1,132 @@
+﻿using Model;
+using System;
+using System.Data.SqlClient;
+using System.Data;
+
+namespace DAL
+{
+    public class TarefaDAL
+    {
+        public Usuario Inserir(Usuario _usuario)
+        {
+            SqlConnection cn = new SqlConnection();
+            try
+            {
+                cn.ConnectionString = Conexao.StringDeConexao;
+                SqlCommand cmd = new SqlCommand();
+                cmd.Connection = cn;
+                cmd.CommandType = CommandType.StoredProcedure;
+                cmd.CommandText = "SP_inserirTarefa";
+
+                SqlParameter pid = new SqlParameter("@Id", SqlDbType.Int);
+                pid.Value = _usuario.Id;
+                cmd.Parameters.Add(pid);
+
+                SqlParameter pid_usuario = new SqlParameter("@Id_usuario", SqlDbType.Int);
+                pid_usuario.Value = _usuario.Ativo;
+                cmd.Parameters.Add(pid_usuario);
+
+                SqlParameter pdescricao = new SqlParameter("@Descricao  ", SqlDbType.VarChar);
+                pdescricao.Value = _usuario.NomeUsuario;
+                cmd.Parameters.Add(pdescricao);
+
+
+                SqlParameter prelatorio = new SqlParameter("@Relatorio", SqlDbType.VarChar);
+                prelatorio.Value = _usuario.Senha;
+                cmd.Parameters.Add(prelatorio);
+
+
+                cn.Open();
+                _usuario.Id = Convert.ToInt32(cmd.ExecuteScalar());
+
+                return _usuario;
+            }
+            catch (SqlException ex)
+            {
+                throw new Exception("Servidor SQL Erro: " + ex.Message);
+            }
+
+            catch (Exception ex)
+            {
+
+                throw new Exception(ex.Message);
+            }
+            finally
+            {
+                cn.Close();
+            }
+        }
+        public DataTable Buscar(string _filtro)
+        {
+            SqlDataAdapter da = new SqlDataAdapter();
+            DataTable dt = new DataTable();
+            SqlConnection cn = new SqlConnection();
+            try
+            {
+                cn.ConnectionString = Conexao.StringDeConexao;
+                SqlCommand cmd = new SqlCommand();
+                da.SelectCommand = cmd;
+                da.SelectCommand.Connection = cn;
+                da.SelectCommand.CommandText = "SP_buscarTarefa";
+                da.SelectCommand.CommandType = CommandType.StoredProcedure;
+
+                SqlParameter pfiltro = new SqlParameter("@filtro", SqlDbType.VarChar);
+                pfiltro.Value = _filtro;
+                da.SelectCommand.Parameters.Add(pfiltro);
+
+                cn.Open();
+                da.Fill(dt);
+                return dt;
+
+            }
+            catch (SqlException ex)
+            {
+                throw new Exception("" + ex.Message);
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+            finally
+            {
+                cn.Close();
+            }
+        }
+        public void Excluir(int _id)
+        {
+            SqlConnection cn = new SqlConnection();
+            try
+            {
+                cn.ConnectionString = Conexao.StringDeConexao;
+                SqlCommand cmd = new SqlCommand();
+                cmd.Connection = cn;
+                cmd.CommandType = CommandType.StoredProcedure;
+                cmd.CommandText = "SP_excluirTarefa";
+                SqlParameter pid = new SqlParameter("@Id", SqlDbType.Int);
+                pid.Value = _id;
+                cmd.Parameters.Add(pid);
+
+                cn.Open();
+                int resultado = cmd.ExecuteNonQuery();
+                if (resultado != 1)
+                {
+                    throw new Exception("Não foi possível excluir o usuário: " +
+                        _id.ToString());
+                }
+            }
+            catch (SqlException ex)
+            {
+
+                throw new Exception("Servidor SQL Erro: " + ex.Message);
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+            finally
+            {
+                cn.Close();
+            }
+        }
+    }
+}
--- Base/DAL/UsuarioDAL.cs
+++ Base/DAL/UsuarioDAL.cs
@@ -7,54 +7,6 @@ namespace DAL
 {
     public class UsuarioDAL
     {
-        public Usuario Inserir(Usuario _usuario)
-        {
-            SqlConnection cn = new SqlConnection();
-            try
-            {
-                cn.ConnectionString = Conexao.StringDeConexao;
-                SqlCommand cmd = new SqlCommand();
-                cmd.Connection = cn;
-                cmd.CommandType = CommandType.StoredProcedure;
-                cmd.CommandText = "SP_InserirUsuario";
-
-                SqlParameter pativo = new SqlParameter("@Ativo", SqlDbType.Bit);
-                pativo.Value = _usuario.Ativo;
-                cmd.Parameters.Add(pativo);
-
-                SqlParameter pnomeUsuario = new SqlParameter("@NomeUsuario", SqlDbType.VarChar);
-                pnomeUsuario.Value = _usuario.NomeUsuario;
-                cmd.Parameters.Add(pnomeUsuario);
-
-
-                SqlParameter psenha = new SqlParameter("@Senha", SqlDbType.VarChar);
-                psenha.Value = _usuario.Senha;
-                cmd.Parameters.Add(psenha);
-
-                SqlParameter pid = new SqlParameter("@Id", SqlDbType.Int);
-                pid.Value = _usuario.Id;
-                cmd.Parameters.Add(pid);
-
-                cn.Open();
-                _usuario.Id = Convert.ToInt32(cmd.ExecuteScalar());
-
-                return _usuario;
-            }
-            catch (SqlException ex)
-            {
-                throw new Exception("Servidor SQL Erro: " + ex.Message);
-            }
-
-            catch (Exception ex)
-            {
-
-                throw new Exception(ex.Message);
-            }
-            finally
-            {
-                cn.Close();
-            }
-        }
         public DataTable Buscar(string _filtro)
         {
             SqlDataAdapter da = new SqlDataAdapter();
@@ -66,7 +18,7 @@ namespace DAL
                 SqlCommand cmd = new SqlCommand();
                 da.SelectCommand = cmd;
                 da.SelectCommand.Connection = cn;
-                da.SelectCommand.CommandText = "SP_BuscarUsuario";
+                da.SelectCommand.CommandText = "SP_buscarUsuario";
                 da.SelectCommand.CommandType = CommandType.StoredProcedure;
                 
                 SqlParameter pfiltro = new SqlParameter("@filtro", SqlDbType.VarChar);
@@ -91,84 +43,6 @@ namespace DAL
                 cn.Close();
             }
         }
-        public Usuario Alterar(Usuario _usuario)
-        {
-            SqlConnection cn = new SqlConnection();
-            try
-            {
-                cn.ConnectionString = Conexao.StringDeConexao;
-                SqlCommand cmd = new SqlCommand();
-                cmd.Connection = cn;
-                cmd.CommandType = CommandType.StoredProcedure;
-                cmd.CommandText = "SP_AlterarUsuario";
-                
-                SqlParameter pid = new SqlParameter("@Id", SqlDbType.Int);
-                pid.Value = _usuario.Id;
-                cmd.Parameters.Add(pid);
 
-                SqlParameter pnomeUsuario = new SqlParameter("@NomeUsuario", SqlDbType.VarChar);
-                pnomeUsuario.Value = _usuario.NomeUsuario;
-                cmd.Parameters.Add(pnomeUsuario);
-                
-                SqlParameter psenha= new SqlParameter("@Senha", SqlDbType.VarChar);
-                psenha.Value = _usuario.Senha;
-                cmd.Parameters.Add(psenha);
-                
-                SqlParameter pativo = new SqlParameter("@Ativo", SqlDbType.Bit);
-                pativo.Value = _usuario.Ativo;
-                cmd.Parameters.Add(pativo);
-
-                cn.Open();
-                cmd.ExecuteNonQuery();
-                return _usuario;
-            }
-
-            catch (SqlException ex)
-            {
-
-                throw new Exception("Servidor SQL erro:" + ex.Message);
-            }
-            catch (Exception ex)
-            {
-
-                throw new Exception(ex.Message);
-            }
-        }
-        public void Excluir(int _id)
-        {
-            SqlConnection cn = new SqlConnection();
-            try
-            {
-                cn.ConnectionString = Conexao.StringDeConexao;
-                SqlCommand cmd = new SqlCommand();
-                cmd.Connection = cn;
-                cmd.CommandType = CommandType.StoredProcedure;
-                cmd.CommandText = "SP_ExcluirUsuario";
-                SqlParameter pid = new SqlParameter("@Id",SqlDbType.Int);
-                pid.Value = _id;
-                cmd.Parameters.Add(pid);
-
-                cn.Open();
-                int resultado = cmd.ExecuteNonQuery();
-                if (resultado != 1)
-                {
-                    throw new Exception("Não foi possível excluir o usuário: " +
-                        _id.ToString());
-                }
-            }
-            catch (SqlException ex)
-            {
-
-                throw new Exception("Servidor SQL Erro: " + ex.Message);
-            }
-            catch (Exception ex)
-            {
-                throw new Exception(ex.Message);
-            }
-            finally
-            {
-                cn.Close();
-            }
-        }
     }
 }
